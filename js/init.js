b7371e6545c9b62638c1c4029340f00f0789b59e
Ayla Dillis <aayladillis@gmail.com>
2020-07-06 22:51:11 -0700

    adding contact page

--- js/init.js
+++ js/init.js
@@ -6,3 +6,18 @@
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+
+// const searchTerm = document.querySelector("#srch-trm");
+const contactButton = document.querySelector("#contact-button");
+const homePage = document.querySelector(".home-page");
+const contactPage = document.querySelector(".contact-page");
+
+let address;
+
+contactButton.addEventListener("click", function(event) {
+  event.preventDefault();
+  // address = searchTerm.value;
+  // console.log(address);
+  homePage.setAttribute("style", "display: none");
+  contactPage.setAttribute("style", "display: block");
+});
